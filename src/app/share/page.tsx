ee2e7792a8a9301430d9a04aa9c8a90b3f2dd1f1
MaryamArif7 <www.maryamarif28242@gmail.com>
2025-02-03 00:49:47 +0500

    final

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -1,12 +1,12 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: " Shared Task",
+  title: "Shared Task",
   description: "This is a shared page for the purpose of the task.",
   openGraph: {
-    title: "Shared Page ",
+    title: "Shared Page",
     description: "This is a shared page for the purpose of the task.",
-    url: "https://rabbito.io/",
+    url: "https://rabbito.io/", 
     images: [
       {
         url: "https://media.licdn.com/dms/image/v2/D4E0BAQHS-3AIdEPh4w/company-logo_200_200/company-logo_200_200/0/1710112143160/rabbitoio_logo?e=1746662400&v=beta&t=GV2yMPGyLqNgE4A-p9BD6CQ4-Tw0maXGjTX08Nj3fWk",
@@ -15,17 +15,14 @@ export const metadata: Metadata = {
         alt: "rabbito",
       },
     ],
-    type: "company",
+    siteName: "Rabbito",
   },
- 
 };
 
 export default function SharePage() {
   return (
     <div style={{ display: "none" }}>
-     This page is only for the task
+      This page is only for the task
     </div>
   );
 }
-
-
